05b7f097932cfa98356efc8be149bed7a7ce614a
THNam <duongtran19964@gmail.com>
2019-01-11 10:35:33 +0700

    Variable caculation

---
+++ roundarea.js
@@ -0,0 +1,6 @@
+const PI = 3.14;
+var r = parseInt(prompt("Nhap ban kinh: "));
+var per = 2 * PI * r;
+var area = PI * Math.pow( r, 2);
+ document.write("Chu vi hinh tron: " + per);
+ document.write("Dien tich hinh tron: "+ area);
